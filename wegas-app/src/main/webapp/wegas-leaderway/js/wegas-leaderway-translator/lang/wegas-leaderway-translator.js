cfbb11606cb015edf978fb78c44ddea9361e0001
Francois-Xavier Aeberhard <fx@red-agent.com>
2012-11-22 13:35:11 +0100

    Signed-off-by: Francois-Xavier Aeberhard <fx@red-agent.com>

--- wegas-app/src/main/webapp/wegas-leaderway/js/wegas-leaderway-translator/lang/wegas-leaderway-translator.js
+++
@@ -1,8 +0,0 @@
-YUI.add("lang/wegas-leaderway-translator", function(Y) {
-    Y.Intl.add("wegas-leaderway-translator", "default",
-        {
-            folder:"Dossier",
-            goodbye: "Au revoir"
-        }
-    );
-});
--- wegas-app/src/main/webapp/wegas-leaderway/js/wegas-leaderway-translator/lang/wegas-leaderway-translator_en.js
+++
@@ -1,8 +0,0 @@
-YUI.add("lang/wegas-leaderway-translator_en", function(Y) {
-    Y.Intl.add("wegas-leaderway-translator", "en",
-        {
-            folder:"Folder",
-            goodbye: "Goodbye"
-        }
-    );
-});
